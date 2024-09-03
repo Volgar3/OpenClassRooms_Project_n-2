d4ff6653aacb936488370a3581e2f942b69d90a8
Aurelien lebray <aurelien.lebray@gmail.com>
2024-09-03 09:21:53 +0200

    lesson 20240903

--- step_1.py
+++ step_1.py
@@ -1,5 +1,16 @@
+from requests import get
+from bs4 import BeautifulSoup
+
 def scrap_book(url):
-    pass
+    response = get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    return {
+        "product_page_url": url,
+        "universal_ product_code": rows[0].find('td').text
+    }
 
 if __name__ == '__main__':
-    scrap_book("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+    result = scrap_book("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+    print(result)
