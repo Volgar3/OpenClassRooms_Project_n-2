b820ce3693dfb8d7ea66b1cfc812d514e02c2a3b
Aurelien lebray <aurelien.lebray@gmail.com>
2024-09-17 13:07:49 +0200

    Lesson 20240917

--- step_1.py
+++ step_1.py
@@ -5,43 +5,45 @@ def scrap_book(url):
     response = get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
- #Récupération du tableau
+    #Récupération du tableau
 
     table = soup.find('table')
-
-#Code upc 
+    
+    #Code upc 
 
     rows = table.find_all('tr')
 
-#Titre 
+    #Titre 
 
     title = soup.find('h1')
 
-#Description du livre
+    #Description du livre
 
     p = soup.find_all('p')[3]
     description_produit = p.text
 
-#Listes des balises td contenenant le prix ttc/htc, nb de livre disponible et le nombre de review 
+    #Listes des balises td contenenant le prix ttc/htc, nb de livre disponible et le nombre de review 
 
     tds = table.find_all('td')
     liste_td = []
     for td in tds: 
         liste_td.append(td)
 
-#Catégorie du livre 
+    #Catégorie du livre 
     div = soup.find('div', class_= "container-fluid page")
     lis = div.find_all('li')
     liste_li = []
     for li in lis:
         liste_li.append(li)
-#Liens des images des livres
-
+    
+    #Liens des images des livres
     image = div.find('img')
+
+    # A rendre dynamique
     lien = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/' + image['src']
 
-# On retourne toutes les informations dans un dictionnaire que l'on rempli en lors du return en assigant une clé à un élément
-    return {
+    # On retourne toutes les informations dans un dictionnaire que l'on rempli en lors du return en assigant une clé à un élément
+    result =  {
         "product_page_url": url,
         "universal_ product_code": rows[0].find('td').text,
         "title": title.text,
@@ -50,9 +52,11 @@ def scrap_book(url):
         "prince_excluding_tax": liste_td[2].text, 
         "Availability": liste_td[5].text,
         "review_rating": liste_td[6].text,
-        "category": liste_li[2].text,
+        "category": liste_li[2].text.strip(),
         "image_url": lien
     }
+    print(result)
+    return result
 
 if __name__ == '__main__':
     result = scrap_book("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
--- step_2.py
+++ step_2.py
@@ -3,42 +3,31 @@ from bs4 import BeautifulSoup
 from requests import get
 
 def scrap_category(url):
-    response = get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    while url: 
+    while url:
+        response = get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
         h3_liste = soup.find_all('h3')
 
         liste_url_livre = []
         for h3 in h3_liste:
         
-            Liste_a = h3.find('a')
-            if Liste_a and 'href' in Liste_a.attrs:
+            liste_a = h3.find('a')
+            if liste_a and 'href' in liste_a.attrs:
             
-                livre_url = 'https://books.toscrape.com/catalogue/' + Liste_a['href'].replace('../../../', '')
+                livre_url = 'https://books.toscrape.com/catalogue/' + liste_a['href'].replace('../../../', '')
            
                 liste_url_livre.append(livre_url)
 
         for url in liste_url_livre:
-            print(url)
-
-
+            scrap_book(url)
 
         li = soup.find('li', class_= 'next')
-        Bouton_next = li.find('a')
-        
-        if Bouton_next:
-            next_page_url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/page-2.html' + Bouton_next['href']
+        if li:
+            bouton_next = li.find('a')
+            next_page_url = f'https://books.toscrape.com/catalogue/category/books/mystery_3/{ bouton_next['href'] }'
             url = next_page_url
         else: 
-            break
+            url = None
 
-    print(url)
-
-
-
-    #scrap_book(url)
-
-    
 if __name__ == '__main__':
     scrap_category('https://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html')
