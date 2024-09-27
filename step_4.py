94426f902285a93c363ab806cb6c9afdfb703a41
Aurelien lebray <aurelien.lebray@gmail.com>
2024-09-27 12:55:48 +0200

    lesson 20240927-1

--- step_4.py
+++ step_4.py
@@ -1,5 +1,7 @@
-def scrap_categories_with_images():
-    pass
+from step_3 import scrap_categories
+
+def scrap_categories_with_images(url):
+    scrap_categories(url, has_images=True)
 
 if __name__ == '__main__':
-    scrap_categories_with_images()
+    scrap_categories_with_images("https://books.toscrape.com/index.html")
